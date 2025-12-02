8acc99f0c2cb28ce34fc389a0d29d15a8d18c835
Ritik Vishwakarma <123971705+Ritikguru41@users.noreply.github.com>
2025-12-02 10:04:40 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,23 +1,15 @@
 import axios from "axios";
 
-
-// Create a reusable axios instance
 const API = axios.create({
-  baseURL: "http://localhost:5000", // Backend URL
+  baseURL: "https://ciphardropv2.onrender.com",  // Render backend URL
 });
-
-
-// --- API FUNCTIONS ---
-
 
 // Create a new file-sharing session (sender)
 export const createSession = (fileMeta) =>
   API.post("/api/create-session", fileMeta);
 
-
 // Check if a session code exists (receiver)
 export const checkSession = (code) =>
   API.post("/api/check-session", { code });
-
 
 export default API;
